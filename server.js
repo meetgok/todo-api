0a786daa6f20a87139b0de239e27590f6e29c43d
Mehmet Gök <meetgok@gmail.com>
2016-03-31 00:35:38 +0300

    post req

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id : 1,
-	description : "lunch",
-	completed : false
-}, {
-	id : 2,
-	description : "market",
-	completed : false
-}, {
-	id : 3,
-	description : "cop",
-	completed : true
-}];
+var todos = [{"id":1,"description":"lunch","completed":false}
+	,{"id":2,"description":"market","completed":false}
+	,{"id":3,"description":"cop","completed":true}];
+var nextId = 0;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('todo api root');
@@ -36,6 +30,13 @@ app.get('/todos/:id', function(req, res) {
 		res.status(404).send('no id');
 	}
 	//return;
+});
+
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	body.id = ++nextId;
+	todos.push(body);
+	res.json(todos);
 });
 
 app.listen(PORT, function() {
