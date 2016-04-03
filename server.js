890e24006ac659d7dffe493e66e1685d4daad704
Mehmet Gök <meetgok@gmail.com>
2016-04-03 22:37:51 +0300

    validation

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{"id":1,"description":"lunch","completed":false}
 	,{"id":2,"description":"market","completed":false}
 	,{"id":3,"description":"cop","completed":true}];
-var nextId = 0;
+var nextId = todos.length;
+
 
 app.use(bodyParser.json());
 
@@ -18,25 +22,37 @@ app.get('/todos', function(req, res) {
 });
 
 app.get('/todos/:id', function(req, res) {
-	var element;
-	todos.forEach(function(item) {
-		if(item.id == req.params.id){
-			element = item;
-		}
-	});
-	if(element){
-		res.json(element);
-	} else {
-		res.status(404).send('no id');
+	var lv_id = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {"id": lv_id});
+	if(matchedTodo){
+		res.json(matchedTodo);
+	} else{
+		res.status(404).send();
 	}
-	//return;
 });
 
 app.post('/todos', function(req, res) {
-	var body = req.body;
-	body.id = ++nextId;
-	todos.push(body);
-	res.json(todos);
+	try{
+		var body = _.pick(req.body, "description", "completed");
+		if (!_.isBoolean(body.completed)) {
+			console.log('completed not boolean');
+			return res.status(400).send();
+		}
+		if (!_.isString(body.description)) {
+			console.log('description not string');
+			return res.status(400).send();
+		}
+		if (body.description.trim().length === 0) {
+			console.log('description length 0');
+			return res.status(400).send();
+		}
+		body.description = body.description.trim();
+		body.id = ++nextId;
+		todos.push(body);
+		res.json(todos);
+	} catch(e){
+
+	}
 });
 
 app.listen(PORT, function() {
